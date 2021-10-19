7cf9c1632283e7ee6336544aca3076d428f46655
John Bristowe <jbristowe@gmail.com>
2021-10-19 11:19:13 +1000

    feat: integrated jest and tests for base cases

---
+++ __tests__/base.tests.ts
@@ -0,0 +1,64 @@
+import { Lexer } from "../src/lexer";
+import { TokenType } from "../src/token";
+
+test("string length of zero", () => {
+    const lexer = new Lexer(``);
+    expect(lexer).not.toBeUndefined();
+
+    let token = lexer.nextToken();
+    expect(token.col).toEqual(1);
+    expect(token.ln).toEqual(1);
+    expect(token.tokenError).toBeUndefined();
+    expect(token.tokenType).toEqual(TokenType.EOF);
+    expect(token.value).toEqual(`EOF`);
+});
+
+test("whitespace string", () => {
+    const lexer = new Lexer(` `);
+    expect(lexer).not.toBeUndefined();
+
+    let token = lexer.nextToken();
+    expect(token.col).toEqual(2);
+    expect(token.ln).toEqual(1);
+    expect(token.tokenError).toBeUndefined();
+    expect(token.tokenType).toEqual(TokenType.EOF);
+    expect(token.value).toEqual(`EOF`);
+});
+
+test("emoji", () => {
+    const lexer = new Lexer(`🐙`);
+    expect(lexer).not.toBeUndefined();
+
+    let token = lexer.nextToken();
+    expect(token.col).toEqual(1);
+    expect(token.ln).toEqual(1);
+    expect(token.tokenError).toBeUndefined();
+    expect(token.tokenType).toEqual(TokenType.SYMBOL);
+    expect(token.value).toEqual(`🐙`);
+
+    token = lexer.nextToken();
+    expect(token.col).toEqual(3);
+    expect(token.ln).toEqual(1);
+    expect(token.tokenError).toBeUndefined();
+    expect(token.tokenType).toEqual(TokenType.EOF);
+    expect(token.value).toEqual(`EOF`);
+});
+
+test("emoji in quotes", () => {
+    const lexer = new Lexer(`"🐙"`);
+    expect(lexer).not.toBeUndefined();
+
+    let token = lexer.nextToken();
+    expect(token.col).toEqual(1);
+    expect(token.ln).toEqual(1);
+    expect(token.tokenError).toBeUndefined();
+    expect(token.tokenType).toEqual(TokenType.STRING);
+    expect(token.value).toEqual(`"🐙"`);
+
+    token = lexer.nextToken();
+    expect(token.col).toEqual(5);
+    expect(token.ln).toEqual(1);
+    expect(token.tokenError).toBeUndefined();
+    expect(token.tokenType).toEqual(TokenType.EOF);
+    expect(token.value).toEqual(`EOF`);
+});
